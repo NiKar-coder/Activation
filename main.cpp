c91b801605a30d9c3cc1eaa3ebc4e84c5037f716
Nikita <NikitaKarataev22@gmail.com>
2026-02-28 19:14:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,9 +15,7 @@ public:
         results.clear();
         for (size_t x = 0; x < inputs.size() && x < values.size(); ++x) {
             double res = inputs[x] * values[x];
-            if (res > -1.0 && res < 1.0) {
-                results.push_back(res);
-            }
+            if (res > -1.0 && res < 1.0) results.push_back(res);
         }
         return results;
     }
